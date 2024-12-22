ca68140b44eea8b07992b6f0f1aada1b4023e485
Wulferis <acer1171090@gmail.com>
2024-12-22 13:26:13 +1100

    Improved Dependency Tracking & Fixed Compile Error

--- Echo/Serializer.cs
+++ Echo/Serializer.cs
@@ -8,6 +8,35 @@ namespace Prowl.Echo;
 
 public static class Serializer
 {
+    /// <summary>
+    /// Since the serializer supports serializing EchoObjects
+    /// Its possible the EchoObject may have more dependencies inside it
+    /// Prowl handles these dependencies with something like:
+    /// public static void GetAllAssetRefsInEcho(EchoObject echo, ref HashSet<Guid> refs)
+    /// {
+    ///     if (echo.TagType == EchoType.List)
+    ///     {
+    ///         foreach (var tag in (List<EchoObject>)echo.Value!)
+    ///             GetAllAssetRefs(tag, ref refs);
+    ///     }
+    ///     else if (echo.TagType == EchoType.Compound)
+    ///     {
+    ///         var dict = (Dictionary<string, EchoObject>)echo.Value!;
+    ///         if (TryGet("$type", out var typeName)) // See if we are an asset ref
+    ///         {
+    ///             if (typeName!.StringValue.Contains(Serializer.DependencyType) && echo.TryGet("AssetID", out var assetId))
+    ///             {
+    ///                 if (Guid.TryParse(assetId!.StringValue, out var id) && id != Guid.Empty)
+    ///                     refs.Add(id);
+    ///             }
+    ///         }
+    ///         foreach (var (_, tag) in dict)
+    ///             GetAllAssetRefs(tag, ref refs);
+    ///     }
+    /// }
+    /// </summary>
+    public static Action<EchoObject, HashSet<Guid>>? GetAllDependencyRefsInEcho { get; set; }
+
     public static IEchoLogger Logger { get; set; } = new NullEchoLogger();
 
 
@@ -42,7 +71,7 @@ public static class Serializer
         {
             EchoObject clone = property.Clone();
             HashSet<Guid> deps = new();
-            clone.GetAllAssetRefs(ref deps);
+            GetAllDependencyRefsInEcho?.Invoke(clone, deps);
             foreach (Guid dep in deps)
                 context.AddDependency(dep);
             return clone;
