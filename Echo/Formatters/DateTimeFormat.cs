b869efa182c5a488e39645ed25da2b767766900c
Wulferis <acer1171090@gmail.com>
2025-04-28 23:30:04 +1000

    Fixed DateTime not supporting being serialized to a object

--- Echo/Formatters/DateTimeFormat.cs
+++ Echo/Formatters/DateTimeFormat.cs
@@ -9,16 +9,42 @@ internal sealed class DateTimeFormat : ISerializationFormat
     public EchoObject Serialize(Type? targetType, object value, SerializationContext context)
     {
         if (value is DateTime date)
-            return new(EchoType.Long, date.ToBinary());
+        {
+            var compound = EchoObject.NewCompound();
+
+            // Handle type information based on TypeMode
+            bool shouldIncludeType = context.TypeMode switch {
+                TypeMode.Aggressive => true, // Always include type information
+                TypeMode.None => false, // Never include type information
+                TypeMode.Auto => targetType == typeof(object) || targetType != typeof(DateTime), // Include type information if target is object or actual type is different
+                _ => true // Default to aggressive for safety
+            };
+            if (shouldIncludeType)
+                compound["$type"] = new(EchoType.String, typeof(DateTime).FullName);
+
+            // Serialize the DateTime properties
+            compound["value"] = new EchoObject(EchoType.Long, date.ToBinary());
+
+            return compound;
+        }
 
         throw new NotSupportedException($"Type '{value.GetType()}' is not supported by DateTimeFormat.");
     }
 
     public object? Deserialize(EchoObject value, Type targetType, SerializationContext context)
     {
-        if (value.TagType != EchoType.Long)
-            throw new Exception($"Expected Long type for DateTime, got {value.TagType}");
-
-        return DateTime.FromBinary(value.LongValue);
+        if (value.TagType == EchoType.Compound)
+        {
+            if (value.TryGet("value", out var dateTag) && dateTag.TagType == EchoType.Long)
+            {
+                long binary = dateTag.LongValue;
+                return DateTime.FromBinary(binary);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid DateTime format.");
+            }
+        }
+        throw new NotSupportedException($"Type '{value.TagType}' is not supported by DateTimeFormat.");
     }
 }
